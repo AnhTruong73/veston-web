a1de2f8cf3815a96559162c893c3a02f329e8334
JayTr <truongdinhanh0703@gmail.com>
2024-07-28 17:06:48 +0700

    123

--- src/web.config.js
+++ src/web.config.js
@@ -1,7 +1,5 @@
 export const APP_ENV = process.env.APP_ENV ?? 'development';
-export const WS_API =
-  process.env.WS_API ??
-  'https://veston-web-git-master-truong-dinh-anhs-projects.vercel.app/api';
+export const WS_API = process.env.WS_API ?? 'https://veston-web.vercel.app/api';
 
 export const COOKIE_KEY = {
   USER_INFO: 'user_info',
