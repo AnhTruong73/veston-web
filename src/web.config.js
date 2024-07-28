63febcc267c1c453ecb5239e03e18a47e36be5f2
JayTr <truongdinhanh0703@gmail.com>
2024-07-28 15:29:09 +0700

    fix api

--- src/web.config.js
+++ src/web.config.js
@@ -1,7 +1,9 @@
-export const APP_ENV = process.env.APP_ENV ?? 'development' 
-export const WS_API = process.env.WS_API ?? 'http://localhost:3000/api'
+export const APP_ENV = process.env.APP_ENV ?? 'development';
+export const WS_API =
+  process.env.WS_API ??
+  'https://veston-web-git-master-truong-dinh-anhs-projects.vercel.app/api';
 
 export const COOKIE_KEY = {
-    USER_INFO: 'user_info',
-    API_TOKEN_KEY: 'token'
-}
+  USER_INFO: 'user_info',
+  API_TOKEN_KEY: 'token',
+};
