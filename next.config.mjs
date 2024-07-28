64e2cab68c933c7ca637e8bd7c8a198f02f9a13e
JayTr <truongdinhanh0703@gmail.com>
2024-07-28 19:03:44 +0700

    add client

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://veston-web.vercel.app',
+            value: '*',
           },
           {
             key: 'Access-Control-Allow-Methods',
