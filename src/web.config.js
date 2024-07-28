60360ba12948defd31ce8edaa66a0c9df75cdf1c
JayTr <truongdinhanh0703@gmail.com>
2024-07-28 23:20:07 +0700

    update api

--- src/web.config.js
+++ src/web.config.js
@@ -1,5 +1,5 @@
 export const APP_ENV = process.env.APP_ENV ?? 'development';
-export const WS_API = process.env.WS_API ?? 'http://localhost:3000/api';
+export const WS_API = process.env.WS_API ?? 'https://veston-web.vercel.app/api';
 
 export const COOKIE_KEY = {
   USER_INFO: 'user_info',
